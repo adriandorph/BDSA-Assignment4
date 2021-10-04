70477678a5b8afd01c367cf1dc5ee2498a28b4dd
addo <addo@itu.dk>
2021-10-04 17:04:56 +0200

    taskRepository

--- Assignment4.Entities/TaskRepository.cs
+++ Assignment4.Entities/TaskRepository.cs
@@ -1,6 +1,37 @@
+using Assignment4.Core;
+using System.Collections.Generic;
 namespace Assignment4.Entities
 {
-    public class TaskRepository
+    public class TaskRepository : ITaskRepository
     {
+        public IEnumerable<TaskDTO> Tasks;
+        public IReadOnlyCollection<TaskDTO> All(){
+            return (IReadOnlyCollection<TaskDTO>)Tasks;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns>The id of the newly created task</returns>
+        public int Create(TaskDTO task){
+            return 0;
+        }
+
+        public void Delete(int taskId){
+            
+        }
+
+        public TaskDetailsDTO FindById(int id){
+            return null;
+        }
+
+        public void Update(TaskDTO task){
+            
+        }
+
+        public void Dispose(){
+            
+        }
     }
 }
