bf00d3e382f63f99ecedb7af0b0ff893288f30af
maibl <mai.blommen@gmail.com>
2021-10-04 17:03:36 +0200

    SQLDb setup v1

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Assignment4.Entities;
 
 namespace Assignment4
 {
@@ -6,7 +10,21 @@ namespace Assignment4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = LoadConfiguration();
+            var connectionString = configuration.GetConnectionString("Kanban");
+
+            var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>().UseSqlServer(connectionString);
+            using var context = new KanbanContext(optionsBuilder.Options);
+        }
+
+        static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>();
+
+            return builder.Build();
         }
     }
 }
